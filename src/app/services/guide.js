f3b31d4cb8c656f6dec46461bcd83da16b27ce44
kenpham4real <khanhphd.robert@gmail.com>
2021-10-01 11:03:19 +0700

    Re-structured folders

---
+++ src/app/services/guide.js
@@ -0,0 +1,14 @@
+// "services" is the place to store all the fetching or operation templates from the client to the server
+
+// Example
+
+/**
+import * as httpClient from "./BaseService";
+import Path from "../constants/PathUrl";
+
+const getAllUsers = () => {
+  return httpClient.get(Path.USER_ALL);
+};
+
+export { getAllUsers };
+*/
