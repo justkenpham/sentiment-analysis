c1e363e7620830f1562a0b034a1e78605eeddb3c
kenpham4real <khanhphd.robert@gmail.com>
2021-11-21 16:17:30 +0700

    Adjust style so that the login form doesn't cover the Product Detail page

--- src/app/App.js
+++ src/app/App.js
@@ -1,54 +1,56 @@
-import './App.css';
-import Header from './components/Header';
-import MainRoute from './routes/MainRoute';
-import React, {useState} from 'react';
-import LoginForm from './components/LoginForm';
+import "./App.css";
+import Header from "./components/Header";
+import MainRoute from "./routes/MainRoute";
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   const adminUser = {
-    email:"1@1.com",
-    password: "123"
-  }
+    email: "1@1.com",
+    password: "123",
+  };
 
-  const [user, setUser] = useState({name: "", email: ""})
+  const [user, setUser] = useState({ name: "", email: "" });
   const [error, setError] = useState("");
 
-  const Login = details => {
+  const Login = (details) => {
     console.log(details);
 
-    if (details.email == adminUser.email && details.password == adminUser.password){
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
       console.log("Logged in");
       setUser({
         name: details.name,
-        email: details.email
+        email: details.email,
       });
-
     } else {
       setError("Details don't match!");
     }
-
-  }
+  };
 
   const Logout = () => {
-    setUser({name: "",email: ""});
-  }
+    setUser({ name: "", email: "" });
+  };
 
   return (
     <div className="App">
-      <Header/>
-      <MainRoute/>
-      {(user.email != "") ? (
+      <Header />
+      <MainRoute />
+      {user.email != "" ? (
         <div className="welcome">
           <h1>Successfully logged in</h1>
-          <h2>User name: <span>{user.name}</span></h2>
+          <h2>
+            User name: <span>{user.name}</span>
+          </h2>
           <button onClick={Logout}>Logout</button>
         </div>
       ) : (
-        <LoginForm Login={Login} error={error}/>
-      ) }
+        <LoginForm Login={Login} error={error} />
+      )}
     </div>
   );
-
 }
 
 export default App;
