3e7f35e058ec1e4d68853903b9e43719580785af
HTD2k1 <superdatlocpro@gmail.com>
2021-11-28 16:59:16 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,17 +20,23 @@ app.get("/", (req, res) => {
     res.json({ message: "This is what the fuck bro." });
 });
 
+const postRouter = require('./app/routes/Comments');
+app.use("/comments", postRouter);
+
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
 
 const db = require("./app/models");
-db.sequelize.sync().then((data) =>{
-    console.log("Successfully!!");
-}).catch((err) => {
-    console.log("Failed!!");
+db.sequelize.sync().then(
+    () => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}.`);
+        })
+    }
+).catch((err) => {
+    console.log(`Failed to run on port ${PORT}.`);
+    console.log(err)
 });
 
 // // drop the table if it already exists
