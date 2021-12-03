d2e3cb7c2764eb771586433a47f28c90d81d0495
kenpham4real <khanhphd.robert@gmail.com>
2021-12-04 00:25:14 +0700

    Update Product List Screen UI

--- client/src/app/models/Product.js
+++ client/src/app/models/Product.js
@@ -1,11 +1,11 @@
 export class Product{
     constructor(product_id, product_name, description, price, image, rating, category_id){
-        this.product_id = product_id;
-        this.product_name = product_name;
-        this.description = description;
-        this.price = price;
-        this.image = image;
-        this.rating = rating;
-        this.category_id = category_id;
+        this.product_id = product_id; // int
+        this.product_name = product_name; //str
+        this.description = description; // str
+        this.price = price; // int
+        this.image = image; // str
+        this.rating = rating; // str
+        this.category_id = category_id; // int
     }
 }
--- client/src/app/pages/ProductList/index.js
+++ client/src/app/pages/ProductList/index.js
@@ -2,47 +2,57 @@ import React, { useEffect } from 'react';
 import { exampleProduct } from '../../constants/Product';
 // import { getAllProducts } from '../../services/Product';
 
+import {allProducts} from '../../data/Product.js'
+
 import "./index.css"
 
 const ProductList = props => {
-    let numberOfProducts = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
 
-    // const onViewProduct = () => {
-
-    // }
+    const onViewProduct = () => {
+        props.history.push("/product-detail")
+    }
 
     // useEffect(() => getAllProducts(),[])
 
-    return (
+    return(
         <div className="pl-container">
             <div className="pl-filter">
                 <p>Filter</p>
-                <div>
+                <div className="pl-select">
                     <select title="Price">
-                        <option>Price</option>
+                        <option>💵 Price</option>
                         <option>More than $10K</option>
                         <option>From $1K to $10K</option>
                         <option>Less than $1K</option>
                     </select>
                     <select title="Rating">
-                        <option>Rating</option>
-                        <option>Good</option>
-                        <option>Normal</option>
-                        <option>Bad</option>
+                        <option>🤔 Rating</option>
+                        <option>😆 Good</option>
+                        <option>🙂 Normal</option>
+                        <option>😒 Bad</option>
                     </select>
+                </div>
+                <div className="pl-btn">    
+                    <div className="pl-btn__login">Login</div>
+                    <div className="pl-btn__signup">Signup</div>
                 </div>
             </div>
             <section className="pl-list">
-                {numberOfProducts.map(() => (
-                    <div className="pl-list__product"
-                    //onClick={onViewProduct}
-                    >
-                        <img src={exampleProduct.image} />
-                        <p>{exampleProduct.product_name}</p>
-                        <p>{exampleProduct.price}</p>
-                        <p>{exampleProduct.rating}</p>
+                {allProducts.map((prod) => (
+                    <div className="pl-list__product" onClick={onViewProduct}>
+                        <img src={prod.image} />
+                        <div>
+                            <div className="pl-list__product__name">
+                                <p>{prod.product_name}</p>
+                            </div>
+                            <div className="pl-list__product__under-name">
+                                <p className="pl-list__product__under-name__price">${prod.price}</p>
+                                <p className="pl-list__product__under-name__rating">{prod.rating}</p>
+                            </div>
+                        </div>
                     </div>
-                ))}
+                ) )}
             </section>
         </div>
     )
