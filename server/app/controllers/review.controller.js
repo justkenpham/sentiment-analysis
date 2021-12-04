43690c36245216ea6ea2b64ebf3ef9cd3f133ced
quang2652001 <quang2652001@gmail.com>
2021-12-04 23:50:39 +0700

    Update review.controller.js

--- server/app/controllers/review.controller.js
+++ server/app/controllers/review.controller.js
@@ -1,4 +1,5 @@
 const db = require('../models/database')
+const sentiAna = require('../controllers/sentiAna.controller');
 //GET
 module.exports.getReview = async (req, res) => {
     console.log("request params  ", req.param)
@@ -19,7 +20,9 @@ module.exports.getReview = async (req, res) => {
 
 exports.postReview = async (req, res) => {
     console.log("request body", req.body)
-    const { review_text, review_time, overall, user_id, product_id } = req.body
+    const { review_text, review_time, user_id, product_id } = req.body
+    let overall = sentiAna.calculateOverall(review_text);
+    console.log(overall);
     let insert_data = [review_text, review_time, overall, user_id, product_id]
     console.log(insert_data)
     //Query
@@ -37,4 +40,32 @@ exports.postReview = async (req, res) => {
             })
         }
     })
+    //Save updated rating to database
+    sentiAna.takeRating(product_id)
+        .then((num_reviews) => {
+            return new Promise((resolve, reject) => {
+                db.query(`SELECT rating AS rating_score FROM product WHERE product_id = ?`, [product_id], async (err, result) => {
+                    if(err){
+                        reject(err);
+                    }
+                    else {
+                        let rating_score = result[0].rating_score;
+                        let difference = overall - rating_score;
+                        rating_score = rating_score + difference/num_reviews;
+                        console.log(rating_score);
+                        resolve(rating_score);
+                    }
+                })
+            })   
+        })   
+        .then((data) => {
+            db.query(`UPDATE product SET rating = ? WHERE product_id = ?`, [data, product_id], (err, result) => {
+                if(err){
+                    console.log("Can not update to the database");
+                }
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
