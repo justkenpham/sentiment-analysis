8de71d9ed03c3d08e3abedc9d004485f13792a29
quang2652001 <quang2652001@gmail.com>
2021-12-04 10:12:02 +0700

    update route

--- server/app/routes/login.route.js
+++ server/app/routes/login.route.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/login.controller');
 
-router.post("/logins", loginController.login);
+router.post("/", loginController.login);
 module.exports = router;
--- server/app/routes/register.route.js
+++ server/app/routes/register.route.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const registerController = require('../controllers/register.controller');
 
-router.post('/registers', registerController.register);
+router.post('/', registerController.register);
 module.exports = router;
