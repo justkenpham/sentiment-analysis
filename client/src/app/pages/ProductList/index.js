a089b668e1a9eb53ebf7fa175cb302fe532a3730
kenpham4real <khanhphd.robert@gmail.com>
2021-12-05 00:14:04 +0700

    Finish Filter functions in Product List Screen

--- client/src/app/pages/ProductList/index.js
+++ client/src/app/pages/ProductList/index.js
@@ -1,38 +1,90 @@
 import React, { useEffect } from 'react';
+import { ProductFilter } from '../../constants/Product';
 import { getAllProducts } from '../../utils/product.utils';
-
+import { QuickSort, SortRating } from '../../utils/sort';
 
 import "./index.css"
 
 const ProductList = props => {
 
     const [productList, setProductList] = React.useState([]);
-
+    const [originalProductList, setOriginalProductList] = React.useState([]);
+    
     const onViewProduct = () => {
         props.history.push("/product-detail")
-
     }
 
     useEffect(() => {
-        getAllProducts().then(res => setProductList(res))
-    }, [])
+        getAllProducts().then(res => {
+            setProductList(res);
+            setOriginalProductList(res);
+        })
+    },[])
 
-    return (
+    const onFilteringProductBasedOnPrice = (e) => {
+        let range = e.target.value;
+        let searchedProducts = [];
+        
+        if(range == ProductFilter.tag.price.Lowest){
+            for(let i = 0; i < originalProductList.length; i++){
+                if(originalProductList[i].price < 100){
+                    searchedProducts.push(originalProductList[i])
+                }
+            }
+            console.log(searchedProducts);
+        }else if(range == ProductFilter.tag.price.Medium){
+            for(let i = 0; i < originalProductList.length; i++){
+                if(originalProductList[i].price > 100 && originalProductList[i].price < 500){
+                    searchedProducts.push(originalProductList[i])
+                }
+            }
+        }else if(range == ProductFilter.tag.price.Biggest){
+            for(let i = 0; i < originalProductList.length; i++){
+                if(originalProductList[i].price > 500){
+                    searchedProducts.push(originalProductList[i])
+                }
+            }
+        }else{
+            let tempList = [].concat(originalProductList);
+            setProductList(tempList);
+            return;
+        }
+        setProductList(searchedProducts);
+    }
+
+    const onFilteringProductBasedOnRating = (e) => {
+        let range = e.target.value;
+        console.log("rating range", range)
+        let searchedProducts = [].concat(originalProductList);
+        console.log("searched Products", searchedProducts)
+
+        if(range !== "🤔 Rating"){
+            SortRating(searchedProducts, range)
+        }
+        else{
+            let tempList = [].concat(originalProductList);
+            setProductList(tempList);
+            return;
+        }
+        console.log("after filter by rating", searchedProducts)
+        setProductList(searchedProducts);
+    }
+
+    return(
         <div className="pl-container">
             <div className="pl-filter">
                 <p>Filter</p>
                 <div className="pl-select">
-                    <select title="Price">
-                        <option>💵 Price</option>
-                        <option>More than $10K</option>
-                        <option>From $1K to $10K</option>
-                        <option>Less than $1K</option>
+                    <select onChange={onFilteringProductBasedOnPrice} title="Price">
+                        <option value="💵 Price">💵 Price</option>
+                        <option value={ProductFilter.tag.price.Biggest}>More than $500</option>
+                        <option value={ProductFilter.tag.price.Medium}>From $100 to $500</option>
+                        <option value={ProductFilter.tag.price.Lowest}>Less than $100</option>
                     </select>
-                    <select title="Rating">
+                    <select onChange={onFilteringProductBasedOnRating} title="Rating">
                         <option>🤔 Rating</option>
-                        <option>😆 Good</option>
-                        <option>🙂 Normal</option>
-                        <option>😒 Bad</option>
+                        <option value={ProductFilter.tag.rating['Highest To Lowest']}>Highest to Lowest</option>
+                        <option value={ProductFilter.tag.rating['Lowest To Highest']}>Lowest to Highest</option>
                     </select>
                 </div>
             </div>
@@ -50,7 +102,7 @@ const ProductList = props => {
                             </div>
                         </div>
                     </div>
-                ))) : null}
+                ))) : null }
             </section>
         </div>
     )
