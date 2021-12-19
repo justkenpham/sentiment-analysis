fce2a7b4e7bae2aa8b8744ee73c4ea364d4204c9
baryenmunich123 <thanhphongth132@gmail.com>
2021-12-19 17:31:30 +0700

    edit home page

--- client/src/app/pages/HomePage.js
+++ client/src/app/pages/HomePage.js
@@ -2,7 +2,9 @@ import React from 'react'
 import "./HomePage.css"
 import AppLogo from '../assets/icon/brand.png'
 import AppImage from '../assets/sentiment-analysis.png'
-import Footer from "./ProductDetail/product-detail-footer"
+import Facebook from '../assets/facebook.png'
+import Ig from '../assets/instagram.png'
+import TW from '../assets/twitter.png'
 
 import { useHistory } from 'react-router-dom'
 
@@ -24,7 +26,12 @@ const HomePage = (props) => {
                     <p>Help customers learn more about the products, so that they can make better decisions on purchasing the products.</p>
                 </div>
                 <img className='image' src={AppImage}/>
-            </div> 
+                <ul class='sci'>
+                <li><a href="https://www.facebook.com/"><img className='fb' href src={Facebook}/></a></li>
+                <li><a href="https://www.instagram.com/"><img className='ig' src={Ig}/></a></li>
+                <li><a href="https://www.pinterest.com/"><img className='tw' src={TW}/></a></li>
+            </ul> 
+            </div>
         </section>
     )
 }
